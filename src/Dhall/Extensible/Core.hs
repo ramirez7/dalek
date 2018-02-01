648efb1c13f553d79931d856bb9ca1bcb83f9276
Armando Ramirez <armando.m.ramirez@gmail.com>
2018-01-31 17:03:39 -0800

    the normalizer shit was broken

--- src/Dhall/Extensible/Core.hs
+++ src/Dhall/Extensible/Core.hs
@@ -26,35 +26,32 @@ import qualified Dhall.Core                as Dh
 -- http://blog.sumtypeofway.com/recursion-schemes-part-41-2-better-living-through-base-functors/
 --
 -- @rec@ is typically kept polymorphic
-newtype Rec s f rec = Rec { unRec :: f (Dh.Expr s (Rec s rec rec)) }
+newtype Rec s f = Rec { unRec :: f (Dh.Expr s (Rec s f)) }
 
-type Rec' s f = Rec s f f
-
-mapRec :: Functor f => (forall a. f a -> g a) -> Rec s f rec -> Rec s g rec
-mapRec f (Rec x) = Rec (f x)
+--mapRec :: Functor f => (forall a. f a -> g a) -> Rec s f -> Rec s g
+--mapRec f (Rec x) = Rec (f x)
 {-
 traverseRec :: (Monad m, Traversable f) => (forall a. f a -> m (g a)) -> Rec s f -> m (Rec s g)
 traverseRec f (Rec x) = (fmap Rec . f) =<< (traverse (traverse (traverseRec f)) x)
 
 deriving instance (Show (f (Dh.Expr s (Rec s f))), Show s) => Show (Rec s f)
 -}
-type OpenNormalizer s (fs :: [* -> *]) = Dh.Normalizer s (Rec' s (Union fs))
+type OpenNormalizer s (fs :: [* -> *]) = Dh.Normalizer s (Rec s (Union fs))
 
+-- This doesn't work. It'll turn to nothing if you mix-and-match embeds -_-
+--
+-- You can't get this type signature usefully. You have to write the Normalizer
+-- generic up-front
 sendNormC :: forall s a fs. Member (Const a) fs => Dh.Normalizer s a -> OpenNormalizer s fs
-sendNormC f = \exprU -> do
-  (matched :: Dh.Expr s a) <- traverse (fmap getConst . prj . unRec) exprU
-  normalized <- f matched
-  pure $ fmap (Rec . inj . Const) normalized
+sendNormC _ = undefined
 
-{-
-Union fs (Rec s (Union fs))
-f (Rec s f)
--}
-sendNormR :: forall s f fs. (Functor f, Member f fs) => (forall rec . Dh.Normalizer s (Rec s f rec)) -> OpenNormalizer s fs
-sendNormR f = \exprU -> do
-  (matched :: Dh.Expr s (Rec s f (Union fs))) <- traverse (fmap Rec . prj . unRec) exprU
-  normalized <- f matched
-  pure $ fmap (mapRec inj) normalized
+infixl 3 .<|>
+-- | TODO: Doc comment
+(.<|>) :: Dh.Normalizer s a -> Dh.Normalizer s a -> Dh.Normalizer s a
+nl .<|> nr = runMaybeT (MaybeT nl <|> MaybeT nr)
+
+sendEmbed :: forall s a fs. Member (Const a) fs => a -> Dh.Expr s (Rec s (Union fs))
+sendEmbed a = Dh.Embed $ Rec $ inj $ Const a
 
 {-
 openNormalizers for extensions should be written like this
@@ -72,11 +69,7 @@ sendNorm f = \exprU -> do
   normalized <- f matched
   pure $ fmap (ulift . Identity) normalized
 
-infixl 3 .<|>
--- | TODO: Doc comment
-(.<|>) :: Dh.Normalizer a -> Dh.Normalizer a -> Dh.Normalizer a
-nl .<|> nr = runMaybeT (MaybeT nl <|> MaybeT nr)
 
-sendEmbed :: forall s a as i. UElem a as i => a -> Dh.Expr s (OpenUnion as)
-sendEmbed a = Dh.Embed $ ulift $ Identity a
+
+
 -}
